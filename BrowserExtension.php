b73812630201a23b30b1728a0f8d10ab7cc1f5bb
Paige Julianne Sullivan <psullivan@jhu.edu>
2023-08-29 14:37:01 -0400

    initial checkin

---
+++ BrowserExtension.php
@@ -0,0 +1,12 @@
+<?php
+namespace METRC\BrowserExtension;
+
+use ExternalModules\AbstractExternalModule;
+use ExternalModules\ExternalModules;
+
+
+class BrowserExtension extends AbstractExternalModule {
+
+
+
+}
