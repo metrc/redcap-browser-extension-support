4744a8fe881b2d6b1259f2f81b2617c750de7cbe
Paige Julianne Sullivan <psullivan@jhu.edu>
2023-09-08 09:42:17 -0400

    fixing bug in grant_all.php script

--- grant_all.php
+++ grant_all.php
@@ -6,6 +6,7 @@ if (!SUPER_USER) die('You must be a super user to run this script');
 
 ?>
     <h5>REDCap Browser Extension Support</h5>
+<p>
 <?php
 $db = new RedCapDB();
 
@@ -17,21 +18,29 @@ while ($row = db_fetch_assoc($q)) {
     $sql = "SELECT api_token, api_export FROM redcap_user_rights WHERE username = ? AND project_id = ?";
     $q2 = db_query($sql, [$row['username'], PROJECT_ID]);
     $row2 = db_fetch_assoc($q2);
-    if ($row2 == NULL) {
+    if (!$row2) {
+        // grant access to this project and generate an API token
         $sql = 'INSERT INTO redcap_user_rights (username, project_id, api_export) VALUES (?, ?, ?)';
         $q3 = db_query($sql, [$row['username'], PROJECT_ID, 1]);
         $db->setAPIToken($row['username'], PROJECT_ID);
-    } elseif ($row2['api_token']) {
+        echo "Granted access to {$row['username']}<br>";
+        continue;   // don't repeat the operations below
+    }
+    if (!$row2['api_token']) {
+        // just generate an API token
         $db->setAPIToken($row['username'], PROJECT_ID);
-    } elseif (!$row['api_export']) {
+        echo "Generated API token for {$row['username']}<br>";
+    }
+    if (!$row2['api_export']) {
+        // just add API export rights
         $sql = 'UPDATE redcap_user_rights SET api_export = ? WHERE username = ? AND project_id = ?';
         $q3 = db_query($sql, [1, $row['username'], PROJECT_ID]);
-        $db->setAPIToken($row['username'], PROJECT_ID);
+        echo "Granted API export rights to {$row['username']}<br>";
     }
 
 }
 ?>
-
-<p>All users granted access to this project and API tokens have been generated.</p>
+</p>
+<p>All operations complete.</p>
 
 <?php require_once APP_PATH_DOCROOT . 'ProjectGeneral/footer.php';
