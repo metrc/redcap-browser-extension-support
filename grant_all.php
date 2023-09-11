890cfd807f6fa2c2437beb0b4a486c10fb314e1b
Paige Julianne Sullivan <psullivan@jhu.edu>
2023-09-11 14:26:26 -0400

    security enhancements

--- grant_all.php
+++ grant_all.php
@@ -23,19 +23,19 @@ while ($row = db_fetch_assoc($q)) {
         $sql = 'INSERT INTO redcap_user_rights (username, project_id, api_export) VALUES (?, ?, ?)';
         $q3 = db_query($sql, [$row['username'], PROJECT_ID, 1]);
         $db->setAPIToken($row['username'], PROJECT_ID);
-        echo "Granted access and generated API token for {$row['username']}<br>";
+        echo "Granted access and generated API token for " . $module->escape($row['username']) . "<br>";
         continue;   // don't repeat the operations below
     }
     if (!$row2['api_token']) {
         // just generate an API token
         $db->setAPIToken($row['username'], PROJECT_ID);
-        echo "Generated API token for {$row['username']}<br>";
+        echo "Generated API token for " . $module->escape($row['username']) . "<br>";
     }
     if (!$row2['api_export']) {
         // just add API export rights
         $sql = 'UPDATE redcap_user_rights SET api_export = ? WHERE username = ? AND project_id = ?';
         $q3 = db_query($sql, [1, $row['username'], PROJECT_ID]);
-        echo "Granted API export rights to {$row['username']}<br>";
+        echo "Granted API export rights to " . $module->escape($row['username']) . "<br>";
     }
 
 }
