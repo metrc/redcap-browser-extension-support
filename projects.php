fe0152b6d795764e089782700bfca17d75d9a354
Paige Julianne Sullivan <psullivan@jhu.edu>
2023-09-11 09:38:25 -0400

    busting cache

--- projects.php
+++ projects.php
@@ -17,5 +17,8 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 header("Content-Type: application/json; charset=UTF-8");
+header("Cache-Control: no-cache, no-store, max-age=0, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: Mon, 01 Jan 1990 00:00:00 GMT");
 echo $json;
 die();
